2b4f42ae33d481f5e8871fd0cc22a153ab05e00f
Aleem Isiaka <aleemisiaka@gmail.com>
2021-09-11 15:38:18 +0100

    Handle Signup form

--- components/Signup.vue
+++ components/Signup.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
+  <form
+    class="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12"
+    v-on:submit.prevent="handleSubmit"
+  >
     <div class="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
       <h1 class="font-bold text-center text-2xl mb-5">Join Env Manager</h1>
       <div class="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
@@ -8,6 +11,7 @@
             >First Name</label
           >
           <input
+            v-model="firstName"
             type="text"
             class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
           />
@@ -15,6 +19,7 @@
             >Last Name</label
           >
           <input
+            v-model="lastName"
             type="text"
             class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
           />
@@ -22,25 +27,37 @@
             >E-mail</label
           >
           <input
-            type="text"
+            v-model="email"
+            type="email"
             class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
           />
           <label class="font-semibold text-sm text-gray-600 pb-1 block"
             >Password</label
           >
           <input
-            type="text"
+            v-model="password"
+            type="password"
             class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
           />
           <label class="font-semibold text-sm text-gray-600 pb-1 block"
             >Confirm Password</label
           >
           <input
-            type="text"
+            v-model="confirmPassword"
+            type="password"
             class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
           />
+          <div class="w-full">
+            {{ error }}
+          </div>
+
+          <div class="w-full">
+            {{ formMessage }}
+          </div>
+
           <button
-            type="button"
+            :disabled="loading"
+            type="submit"
             class="
               transition
               duration-200
@@ -82,5 +99,96 @@
         </div>
       </div>
     </div>
-  </div>
+  </form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      password: '',
+      confirmPassword: '',
+      email: '',
+      loading: false,
+      formMessage: '',
+    }
+  },
+  methods: {
+    handleSubmit() {
+      if (this.error) {
+        return
+      }
+
+      // What is this?
+
+      // Let's use fetch to push the details
+      const body = {
+        firsName: this.firstName,
+        lastName: this.lastName,
+        email: this.email,
+        password: this.password,
+      }
+
+      this.loading = true
+
+      fetch('http://localhost:3200/users/signup', {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then(async (res) => {
+          const resp = await res.json()
+          this.loading = false
+
+          if (res.ok) {
+            this.clearForm()
+            this.formMessage = 'Registration Successful. Proceed to login'
+          } else {
+            const msg = resp.message
+            this.formMessage = Array.isArray(msg) ? resp.message[0] : msg
+          }
+        })
+        .catch((err) => {
+          this.loading = false
+          this.formMessage = 'An error occured. Please try again.'
+        })
+    },
+    clearForm() {
+      this.lastName = ''
+      this.firstName = ''
+      this.email = ''
+      this.password = ''
+      this.confirmPassword = ''
+    },
+  },
+  computed: {
+    error() {
+      if (!this.firstName) {
+        return 'First Name is required'
+      }
+
+      if (!this.lastName) {
+        return 'Last Name is required'
+      }
+
+      if (!this.email) {
+        return 'Email is required'
+      }
+
+      if (!this.password) {
+        return 'Password is required'
+      }
+
+      if (this.password && this.password !== this.confirmPassword) {
+        return 'Passwords do not match'
+      }
+
+      return ''
+    },
+  },
+}
+</script>
